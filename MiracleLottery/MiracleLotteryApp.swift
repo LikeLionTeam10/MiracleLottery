ce6b1d689e6319c3b154e42de271e666c364f252
CodingGorani <tjr2922@gmail.com>
2022-10-23 22:12:54 +0900

    Initial Commit

---
+++ MiracleLottery/MiracleLotteryApp.swift
@@ -0,0 +1,17 @@
+//
+//  MiracleLotteryApp.swift
+//  MiracleLottery
+//
+//  Created by Martin on 2022/10/21.
+//
+
+import SwiftUI
+
+@main
+struct MiracleLotteryApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
